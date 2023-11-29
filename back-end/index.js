f07dc902337d253c033df0714379fedb3efbf256
Aishwarya Arun <jj4245888@gmail.com>
2023-11-29 19:29:28 +0530

    error in /

--- back-end/index.js
+++ back-end/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/',userRouter);
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../front-end/dist')));
 
 app.use(express.static('public'));
 
