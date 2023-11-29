a6223577a0a1d7b5c81c8abeba6658f011c2a22a
Aishwarya Arun <jj4245888@gmail.com>
2023-11-29 20:23:37 +0530

    error in /

--- back-end/index.js
+++ back-end/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/',userRouter);
-app.use(express.static(path.join(__dirname, 'front-end', 'dist')));
+app.use(express.static(path.join(__dirname,'..', 'front-end', 'dist')));
 
 
 
@@ -29,7 +29,7 @@ app.use(express.static('public'));
 
 // Handle all routes by serving the index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front-end', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname,'..', 'front-end', 'dist', 'index.html'));
 });
 // DB Config
 const db = process.env.mongodb;
